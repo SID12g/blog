3b74c1b6ba8ec8c7877d91422c7707dbc9679f31
sid12g <sead12g@gmail.com>
2025-09-24 22:36:23 +0900

    fead: add tags in post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,10 @@ export const metadata = {
 export default function Page() {
   return (
     <section>
-      <h1 className="font-semibold text-2xl mb-8 tracking-tighter">My Blog</h1>
-      <p className="mb-4">{`테스트`}</p>
+      <p className="mb-4">
+        {`안녕하세요, 조성민입니다.` +
+          `글 쓰는 것에 큰 재주가 있는 것은 아니지만, 세상에 Contribute 하고 싶은 마음으로 글을 작성해보려고 합니다.`}
+      </p>
       <BlogPosts />
     </section>
   );
