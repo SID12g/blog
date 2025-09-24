c536e36c75318ca8e57fd25bbdf3b8e2fab195a6
sid12g <sead12g@gmail.com>
2025-09-25 08:08:04 +0900

    fix: lint error & add next config file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,9 @@ export const metadata = {
 export default function Page() {
   return (
     <section>
+      <h1 className="title font-semibold text-2xl tracking-tighter mb-6">
+        Posts
+      </h1>
       <p className="mb-4">
         {`안녕하세요, 조성민입니다.` +
           `글 쓰는 것에 큰 재주가 있는 것은 아니지만, 세상에 Contribute 하고 싶은 마음으로 글을 작성해보려고 합니다.`}
