a4be3adf8c6ec76e4fd0a359bef4c693126e7f4a
sid12g <sead12g@gmail.com>
2025-09-24 21:10:50 +0900

    init

--- src/app/api/posts/article/get.ts
+++
@@ -1,8 +0,0 @@
-import { articleContents } from "@/utils/article/getPosts";
-import { NextResponse } from "next/server";
-
-export default function GET() {
-  const sanitizedContents = articleContents.map(({ content, ...rest }) => rest);
-
-  return NextResponse.json(sanitizedContents);
-}
