d473d10efd782338a4f60b8150c47ef027c69d27
sid12g <sead12g@gmail.com>
2025-09-25 12:17:08 +0900

    fix: replace background image links with a HeroSection component and introduce filtered posts list component

--- src/app/tags/[tag]/page.tsx
+++ src/app/tags/[tag]/page.tsx
@@ -1,8 +1,9 @@
 import { notFound } from "next/navigation";
-import Link from "next/link";
 import { getBlogPosts } from "@/utils";
 import Tags from "@/components/tags";
-import Image from "next/image";
+import HeroSection from "@/components/hero-section";
+import PostsList from "@/components/filtered-posts-list";
+import FilterPostSList from "@/components/filtered-posts-list";
 
 export async function generateStaticParams() {
   let posts = getBlogPosts();
@@ -45,39 +46,9 @@ export default async function TagDetailPage({ params }) {
 
   return (
     <section>
-      <Link
-        href="https://www.youtube.com/watch?v=DXvjwv_9yHU&list=LL&index=4"
-        target="_blank"
-      >
-        <Image
-          src="/background.png"
-          alt="Background"
-          width={544}
-          height={240}
-          quality={95}
-          priority
-          className="w-full h-auto"
-        />
-      </Link>
+      <HeroSection />
       <Tags selectedTag={decodeURIComponent(tag)} />
-      <div>
-        {filtered.map((post) => (
-          <Link
-            key={post.slug}
-            className="group flex flex-col space-y-1 mb-1 -mx-2 px-2 py-1 rounded-md transition hover:bg-neutral-100 dark:hover:bg-neutral-900"
-            href={`/${post.slug}`}
-          >
-            <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 group-hover:text-neutral-700 dark:group-hover:text-neutral-300 transition-colors w-[100px] tabular-nums">
-                {post.metadata.publishedAt}
-              </p>
-              <p className="text-neutral-900 dark:text-neutral-100 tracking-tight group-hover:underline group-hover:decoration-neutral-400 group-hover:underline-offset-2">
-                {post.metadata.title}
-              </p>
-            </div>
-          </Link>
-        ))}
-      </div>
+      <FilterPostSList posts={filtered} />
     </section>
   );
 }
