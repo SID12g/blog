53c599faa24a75e4e33fc9694e7a4ca2f710fb30
sid12g <sead12g@gmail.com>
2025-09-25 09:14:43 +0900

    style: adjust footer margin

--- src/components/nav.tsx
+++ src/components/nav.tsx
@@ -2,16 +2,16 @@ import Link from "next/link";
 
 const navItems = {
   "/": {
-    name: "Home",
+    name: "🐱",
   },
   "/tags": {
-    name: "Tags",
+    name: "🏷️",
   },
 };
 
 export function Navbar() {
   return (
-    <aside className="-ml-[8px] mb-16 tracking-tight">
+    <aside className="-ml-[8px] mb-8 tracking-tight">
       <div className="lg:sticky lg:top-20">
         <nav
           className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative"
