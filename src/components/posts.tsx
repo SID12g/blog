6bf731d5d61c15b1db56ce4b26fc6375f1b773ee
sid12g <sead12g@gmail.com>
2026-04-11 13:35:03 +0900

    feat: add new blog posts and wrap post date in container for layout alignment

--- src/components/posts.tsx
+++ src/components/posts.tsx
@@ -22,9 +22,11 @@ export function BlogPosts() {
             href={`/${post.slug}`}
           >
             <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-              <p className="text-neutral-600 dark:text-neutral-400 group-hover:text-neutral-700 dark:group-hover:text-neutral-300 transition-colors w-[100px] tabular-nums">
-                {post.metadata.publishedAt}
-              </p>
+              <div>
+                <p className="text-neutral-600 dark:text-neutral-400 group-hover:text-neutral-700 dark:group-hover:text-neutral-300 transition-colors w-[100px] tabular-nums">
+                  {post.metadata.publishedAt}
+                </p>
+              </div>
               <p className="text-neutral-900 dark:text-neutral-100 tracking-tight group-hover:underline group-hover:decoration-neutral-400 group-hover:underline-offset-2">
                 {post.metadata.title}
               </p>
